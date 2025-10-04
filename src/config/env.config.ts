222ee037c89e87081327cd328dc746fac43894aa
lesquel <lesquel1319@gmail.com>
2025-10-03 19:49:09 -0500

    feat: update environment configuration and validation schema to include PGPORT and NODE_ENV

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,8 +1,10 @@
 export const envConfig = () => ({
   PGHOST: process.env.PGHOST,
+  PGPORT: process.env.PGPORT,
   PGDATABASE: process.env.PGDATABASE,
   PGUSER: process.env.PGUSER,
   PGPASSWORD: process.env.PGPASSWORD,
   PGSSLMODE: process.env.PGSSLMODE,
   PGCHANNELBINDING: process.env.PGCHANNELBINDING,
+  NODE_ENV: process.env.NODE_ENV,
 });
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,9 +2,11 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
   PGHOST: Joi.string().required(),
+  PGPORT: Joi.number().port().required(),
   PGDATABASE: Joi.string().required(),
   PGUSER: Joi.string().required(),
   PGPASSWORD: Joi.string().required(),
   PGSSLMODE: Joi.string().required(),
   PGCHANNELBINDING: Joi.string().required(),
+  NODE_ENV: Joi.string().valid('development', 'test', 'production').default('development'),
 });
