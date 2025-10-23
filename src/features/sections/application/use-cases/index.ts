52d8d9df5b5b4868bb3b96b2d199fcf09c8716d2
lesquel <lesquel1319@gmail.com>
2025-10-23 14:02:09 -0500

    feat(analytics): implement GetSectionAnalytics use case and repository for section analytics

--- src/features/sections/application/use-cases/index.ts
+++ src/features/sections/application/use-cases/index.ts
@@ -4,3 +4,4 @@ export * from './find-section.use-case';
 export * from './list-sections.use-case';
 export * from './list-restaurant-sections.use-case';
 export * from './update-section.use-case';
+export * from './get-section-analytics.use-case';
