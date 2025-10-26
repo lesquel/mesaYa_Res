31508178c17d3e926d964ec1b67cef7871f683db
lesquel <lesquel1319@gmail.com>
2025-10-26 13:48:22 -0500

    feat(auth): integrate ListUsersUseCase into Auth module

--- src/features/auth/application/use-cases/list-users.use-case.ts
+++ src/features/auth/application/use-cases/list-users.use-case.ts
@@ -1,15 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
 import { UseCase } from '@shared/application/ports/use-case.port';
 import type { ListUsersQuery } from '../dto/input/list-users.query';
 import { PaginatedResult } from '@shared/application/types/pagination';
 import { AuthUser } from '../../domain/entities/auth-user.entity';
 import type { AuthUserRepositoryPort } from '../ports/user.repository.port';
+import { AUTH_USER_REPOSITORY } from '@features/auth/auth.tokens';
 
 export type PaginatedUserDomainResult = PaginatedResult<AuthUser>;
 
+@Injectable()
 export class ListUsersUseCase
   implements UseCase<ListUsersQuery, PaginatedUserDomainResult>
 {
-  constructor(private readonly users: AuthUserRepositoryPort) {}
+  constructor(
+    @Inject(AUTH_USER_REPOSITORY)
+    private readonly users: AuthUserRepositoryPort,
+  ) {}
 
   async execute(query: ListUsersQuery): Promise<PaginatedUserDomainResult> {
     return this.users.paginate(query);
