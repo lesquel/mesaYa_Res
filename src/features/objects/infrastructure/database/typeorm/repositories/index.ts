1a54f0d395ffefb5ae372fe9fb7582a87ffecd07
GINGERPEREZ <gingerperez160@gmail.com>
2025-11-07 18:33:00 -0500

    refactor(index.ts): add file extensions to exports for consistency

--- src/features/objects/infrastructure/database/typeorm/repositories/index.ts
+++ src/features/objects/infrastructure/database/typeorm/repositories/index.ts
@@ -1,2 +1,2 @@
-export * from './graphic-object-typeorm.repository';
-export * from './graphic-object-analytics-typeorm.repository';
+export * from './graphic-object-typeorm.repository.js';
+export * from './graphic-object-analytics-typeorm.repository.js';
