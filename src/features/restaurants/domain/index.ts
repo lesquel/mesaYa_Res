4863bc78526044fad53c458e84266e74756414e3
GINGERPEREZ <gingerperez160@gmail.com>
2025-11-07 18:53:03 -0500

    refactor: update imports to include file extensions for consistency

--- src/features/restaurants/domain/index.ts
+++ src/features/restaurants/domain/index.ts
@@ -1,3 +1,3 @@
-export * from './entities';
-export * from './errors';
-export * from './types';
+export * from './entities/index.js';
+export * from './errors/index.js';
+export * from './types/index.js';
