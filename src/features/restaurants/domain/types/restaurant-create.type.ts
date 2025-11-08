e1981154178b59c23c818de9d3e29b7277431f6b
GINGERPEREZ <gingerperez160@gmail.com>
2025-11-08 13:39:59 -0500

    refactor: remove file extensions from imports for consistency

--- src/features/restaurants/domain/types/restaurant-create.type.ts
+++ src/features/restaurants/domain/types/restaurant-create.type.ts
@@ -1,4 +1,4 @@
-import type { RestaurantDay } from '../entities/values/restaurant-day.js';
+import type { RestaurantDay } from '../entities/values/restaurant-day';
 
 export interface RestaurantCreate {
   name: string;
