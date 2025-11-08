5161f6a0a9b0807949daec269f6cee9275771b5f
GINGERPEREZ <gingerperez160@gmail.com>
2025-11-07 19:00:15 -0500

    refactor(restaurant-domain-repository.port.ts): add file extension to import for consistency

--- src/features/restaurants/domain/repositories/restaurant-domain-repository.port.ts
+++ src/features/restaurants/domain/repositories/restaurant-domain-repository.port.ts
@@ -1,4 +1,4 @@
-import { RestaurantEntity } from '../entities/restaurant.entity';
+import { RestaurantEntity } from '../entities/restaurant.entity.js';
 
 export abstract class IRestaurantDomainRepositoryPort {
   abstract save(restaurant: RestaurantEntity): Promise<RestaurantEntity>;
