e3c10806f6e6d19fad88fc34003c7ffa345f6e67
GINGERPEREZ <gingerperez160@gmail.com>
2025-11-08 13:26:51 -0500

    refactor(index.ts): remove file extensions from exports for consistency

--- src/features/images/application/dto/index.ts
+++ src/features/images/application/dto/index.ts
@@ -1,3 +1,3 @@
-export * from './input/index.js';
-export * from './output/index.js';
-export * from './analytics/index.js';
+export * from './input/index';
+export * from './output/index';
+export * from './analytics/index';
