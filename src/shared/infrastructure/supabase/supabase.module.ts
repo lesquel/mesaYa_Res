a8a9542e47041235839e5895c77e2227d7dc1349
GINGERPEREZ <gingerperez160@gmail.com>
2025-11-08 13:00:05 -0500

    refactor(supabase.module.ts): remove file extension from SupabaseStorageService import for consistency

--- src/shared/infrastructure/supabase/supabase.module.ts
+++ src/shared/infrastructure/supabase/supabase.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { SupabaseStorageService } from './supabase.storage.service.js';
+import { SupabaseStorageService } from './supabase.storage.service';
 
 @Module({
   imports: [ConfigModule],
