eb3d4d5d93db40656a3f5165a3636748ce96d14c
StevSant <Bryanmenoscal2005@gmail.com>
2025-11-14 19:15:22 -0500

    feat: implement delete and update owner reservation use cases with corresponding DTOs and controller methods

---
+++ src/features/reservation/application/dto/input/update-owner-reservation.dto.ts
@@ -0,0 +1,11 @@
+﻿import { PartialType, PickType } from '@nestjs/mapped-types';
+import { CreateReservationDto } from './create-reservation.dto';
+
+export class UpdateOwnerReservationDto extends PartialType(
+  PickType(CreateReservationDto, ['reservationDate', 'reservationTime', 'numberOfGuests'] as const),
+) {}
+
+export type UpdateOwnerReservationCommand = UpdateOwnerReservationDto & {
+  reservationId: string;
+  ownerId: string;
+};
